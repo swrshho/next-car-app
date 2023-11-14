2a734eda222061ed8be3cc8cb5d4dde136fb7b3e
swrshho <swrshho@gmail.com>
2023-11-14 12:11:13 +0330

    init

---
+++ utils/index.ts
@@ -0,0 +1,44 @@
+import { FilterProps } from '@/types'
+
+export const calculateCarRent = (city_mpg: number, year: number) => {
+	const basePricePerDay = 50 // Base rental price per day in dollars
+	const mileageFactor = 0.1 // Additional rate per mile driven
+	const ageFactor = 0.05 // Additional rate per year of vehicle age
+
+	// Calculate additional rate based on mileage and age
+	const mileageRate = city_mpg * mileageFactor
+	const ageRate = (new Date().getFullYear() - year) * ageFactor
+
+	// Calculate total rental rate per day
+	const rentalRatePerDay = basePricePerDay + mileageRate + ageRate
+
+	return rentalRatePerDay.toFixed(0)
+}
+
+export async function fetchCars(filters: FilterProps) {
+	const { manufacturer, model, year, limit, fuel } = filters
+
+	const headers = {
+		'X-RapidAPI-Key': '96d13681cdmsh130d0009d8b2849p18b3bbjsncd1fd39642b4',
+		'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com',
+	}
+
+	const response = await fetch(
+		`https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`,
+		{ headers }
+	)
+
+	const result = await response.json()
+
+	return result
+}
+
+export const updateSearchParams = (type: string, value: string) => {
+	const searchParams = new URLSearchParams(window.location.search)
+
+	searchParams.set(type, value)
+
+	const newPathName = `${window.location.pathname}?${searchParams.toString()}`
+
+	return newPathName
+}
